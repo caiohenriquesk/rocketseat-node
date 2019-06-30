9797f86ddbed38c5b5a1ef98b6be30faaa6868f4
caiohenriquesk <caiohenriquesk@gmail.com>
2019-06-30 15:54:31 -0300

    chore: continuação da aula query & route params

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const express = require('express');
+const express = require("express");
+
+const server = express();
+
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = {  }
+
+server.get("/users/:id", (req, res) => {
+  //return res.send("Hello World!");
+
+  //http://localhost:3000/users?nome=caio
+  //const nome = req.query.nome;
+  //return res.json({ message: `Hello ${nome}!` });
+
+  //const id = req.params.id;
+  const { id } = req.params;
+  return res.json({ message: `Buscando o usuário ${id}` });
+});
+
+// localhost:3000/teste
+server.listen(3000);
