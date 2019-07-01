40a5875cdf2656303bcdceb183ac2c4ce6ae11aa
caiohenriquesk <caiohenriquesk@gmail.com>
2019-06-30 23:44:29 -0300

    chore: final do app feito para aula de node

--- index.js
+++ index.js
@@ -2,20 +2,77 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = {  }
 
-server.get("/users/:id", (req, res) => {
-  //return res.send("Hello World!");
+const users = ["Diego", "Cláudio", "Victor"];
 
-  //http://localhost:3000/users?nome=caio
-  //const nome = req.query.nome;
-  //return res.json({ message: `Hello ${nome}!` });
+//middleware - global (executa ao chamar qualquer URL)
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
 
-  //const id = req.params.id;
-  const { id } = req.params;
-  return res.json({ message: `Buscando o usuário ${id}` });
+  next();
+
+  console.timeEnd("Request");
+});
+
+//middleware - local
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "User name is required" });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+
+  if (!user) {
+    return res.status(400).json({ error: "User invalid" });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  //const { index } = req.params;
+  return res.json(req.user);
+});
+
+server.post("/users", checkUserExists, (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", checkUserInArray, checkUserExists, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", checkUserInArray, (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 // localhost:3000/teste
